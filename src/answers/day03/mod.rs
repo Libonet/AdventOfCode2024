1b19769000752a7b66e19d66aa05ab0dd46c478a
Santiago Libonati <santiago.libonati@gmail.com>
2024-12-04 02:39:16 -0300

    Completed day 3 part 2

--- src/answers/day03/mod.rs
+++ src/answers/day03/mod.rs
@@ -29,7 +29,7 @@ impl Scanner {
     }
 
     pub fn dont(&mut self) {
-        self.enabled = true;
+        self.enabled = false;
     }
 
     /// get next char without advancing the cursor
@@ -148,57 +148,75 @@ fn part1() -> Result<(), io::Error> {
 
 fn parse_do(scanner: &mut Scanner) {
     if !scanner.take(&'d') {
+        scanner.pop();
         return;
     }
     if !scanner.take(&'o') {
+        scanner.pop();
         return;
     }
     if !scanner.take(&'(') {
+        scanner.pop();
         return;
     }
     if !scanner.take(&')') {
+        scanner.pop();
         return;
     }
     scanner.r#do();
 }
 
-fn parse_dont(scanner: &mut Scanner) {
+fn parse_dont(scanner: &mut Scanner) -> bool {
     if !scanner.take(&'d') {
-        return;
+        return false;
     }
     if !scanner.take(&'o') {
-        return;
+        return false;
+    }
+    if !scanner.take(&'n') {
+        return false;
+    }
+    if !scanner.take(&'\'') {
+        return false;
+    }
+    if !scanner.take(&'t') {
+        return false;
     }
     if !scanner.take(&'(') {
-        return;
+        return false;
     }
     if !scanner.take(&')') {
-        return;
+        return false;
     }
     scanner.dont();
+    true
+}
+
+fn parse_text(text: &str) -> i32{
+    let mut scanner = Scanner::new(text);
+    let mut sum: i32 = 0;
+
+    while !scanner.is_done() {
+        if scanner.enabled() {
+            if !parse_dont(&mut scanner) {
+                if let Some(num) = parse_mul(&mut scanner) {
+                    sum += num;
+                }
+            }
+        } else {
+            parse_do(&mut scanner);
+        }
+    }
+
+    sum
 }
 
 fn part2() -> Result<(), io::Error> {
     let contents = read_to_string("input/day03.txt")?;
 
-    let result: i32 = contents.lines()
-        .map(|line| {
-            let mut scanner = Scanner::new(line);
-            let mut vec: Vec<i32> = Vec::new();
-            while !scanner.is_done() {
-                if scanner.enabled {
-                    parse_dont(&mut scanner);
-                    if let Some(num) = parse_mul(&mut scanner) {
-                        vec.push(num);
-                    }
-                } else {
-                    parse_do(&mut scanner);
-                }
-            }
-            vec
-        })
-        .reduce(|a,b| [a, b].concat()).unwrap_or(vec![0]).iter()
-        .sum();
+    let text = contents.lines().collect::<Vec<&str>>().join(", ");
+
+    let result: i32 = parse_text(&text);
 
     println!("part1 result = {result}");
 
