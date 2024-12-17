2ef532982bf982fb587dcc7eaf894c82ae47d3cb
Santiago Libonati <santiago.libonati@gmail.com>
2024-12-17 12:53:34 -0300

    feat: Added some debugging macros

---
+++ src/debug_print.rs
@@ -0,0 +1,27 @@
+#[macro_export]
+macro_rules! debug_print {
+    ($($arg:tt)*) => {
+        (#[cfg(debug_assertions)] print!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! debug_println {
+    ($($arg:tt)*) => {
+        (#[cfg(debug_assertions)] println!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! debug_eprint {
+    ($($arg:tt)*) => {
+        (#[cfg(debug_assertions)] eprint!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! debug_eprintln {
+    ($($arg:tt)*) => {
+        (#[cfg(debug_assertions)] eprintln!($($arg)*))
+    };
+}
